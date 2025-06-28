f52e4cf33417f6f890d8f4bbe06379899df5cc01
Zain676 <mzainq9@gmail.com>
2025-06-28 12:56:33 +0500

    initial commit

--- app.js
+++ app.js
@@ -50,4 +50,6 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => console.log(`Server running on ${HOST}:${PORT}`));
